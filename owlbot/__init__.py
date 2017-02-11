43babc216c5905c538105898889c07e0845e9c27
mugwort_rc <mugwort.rc@gmail.com>
2017-02-11 18:21:18 +0900

    Update owlbot.get()

--- owlbot/__init__.py
+++ owlbot/__init__.py
@@ -4,7 +4,16 @@ from . import archive
 from . import useragent
 
 
-def get(url, headers={}):
+def get(url, headers={}, comment=None):
+    """
+    :type url: str
+    :type headers: dict of (str, str)
+    :type comment: str
+    """
     if "user-agent" not in set(x.lower() for x in headers.keys()):
-        headers["user-agent"] = useragent.UA_DEFAULT
+        if comment is None:
+            ua = useragent.UA_DEFAULT
+        else:
+            ua = useragent.UA_BASE.format(comment=" "+comment)
+        headers["user-agent"] = ua
     return archive.download("GET", url, headers)
