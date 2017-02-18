1403985f90411b3c8f1b5d5394932b4a5a4938f5
mugwort_rc <mugwort.rc@gmail.com>
2017-02-18 16:41:14 +0900

    Update

--- owlbot/analyzer.py
+++ owlbot/analyzer.py
@@ -8,6 +8,9 @@ def extract_document_link(content):
     :type content: str
     :rtype: list of str
     """
+    if not content:
+        return []
+
     dom = html.fromstring(content)
     link_hrefs = dom.xpath("//link/@href")
     script_srcs = dom.xpath("//script/@src")
